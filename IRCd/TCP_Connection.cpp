73a5554af5b2462e3dd6068d5f59a939a84a3ede
Alex Turner <bluechill@me.com>
2014-04-02 11:42:09 -0400

    Boost ASIO C++11 echo server commit.

---
+++ IRCd/TCP_Connection.cpp
@@ -0,0 +1,9 @@
+//
+//  TCP_Connection.cpp
+//  IRCd
+//
+//  Created by Alex Turner on 3/31/14.
+//  Copyright (c) 2014 EvilZone. All rights reserved.
+//
+
+#include "TCP_Connection.h"
--- IRCd/main.cpp
+++ IRCd/main.cpp
@@ -7,12 +7,30 @@
 //
 
 #include <iostream>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+
+#include "TCP_Server.h"
+
+using boost::asio::ip::tcp;
 
 int main(int argc, const char * argv[])
 {
+	try
+	{
+		boost::asio::io_service io_service;
 
-	// insert code here...
-	std::cout << "Hello, World!\n";
+		IRC_Server s(io_service, 31337);
+
+		io_service.run();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
+
     return 0;
 }
 
